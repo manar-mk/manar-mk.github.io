cbfbe174f56017052dd26da6cf96c6b6f95fa8b1
Manar Kurmanov <kurmanov.manar@gmail.com>
2014-12-23 02:38:29 +0600

    automated rgb

--- js/demo.js
+++ js/demo.js
@@ -1,17 +1,20 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
 $(document).ready(function() {
     $('#particles').particleground({
         dotColor: '#aaaaaa',
         lineColor: '#aaaaaa'
     });
-    $('.intro').css({
-    	'left':	(($(window).width()-$('.intro').width())/2)/$(window).width()*100+"%",
-    	'top':	(($(document).height()-$('.intro').height())/2)/$(document).height()*150+"%"
-    });
+    
     var scene = document.getElementById('scene');
+    
+    $('.js-first').clone().addClass('r').removeClass('js-first').removeClass('hidden').attr('data-depth','0.2').appendTo(scene);
+    $('.js-first').clone().addClass('g').removeClass('js-first').removeClass('hidden').attr('data-depth','0.4').appendTo(scene);
+    $('.js-first').clone().addClass('b').removeClass('js-first').removeClass('hidden').attr('data-depth','0.6').appendTo(scene);
+    $('.js-first').clone().addClass('main').removeClass('js-first').removeClass('hidden').attr('data-depth','0.8').appendTo(scene);
+
+    $('.intro').css({
+        'left': (($(window).width()-$('.intro').width())/2)/$(window).width()*100+"%",
+        'top':  (($(document).height()-$('.intro').height())/2)/$(document).height()*150+"%"
+    });
+
     var parallax = new Parallax(scene);
 });
