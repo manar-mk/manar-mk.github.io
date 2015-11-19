3b62809218558837e2e4c0a05ae387deeded3c01
Manar Kurmanov <kurmanov.manar@gmail.com>
2015-11-20 03:24:55 +0600

    taptim parallax

--- taptim/js/main.js
+++ taptim/js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
         event.preventDefault();
         scrollPage($(this).attr('href'));
     });
+     $(".js-fancybox").fancybox({
+        padding: 0
+    });
     function scrollPage(section) {
         $("html, body").animate({
             scrollTop: $(section).offset().top - 130
--- taptim/js/tabs.js
+++ taptim/js/tabs.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
 	var tabs = $('.js-tab');
 	var tabButton = $('.js-tabs-button');
-
+	
 	tabButton.click(function(event) {
 		tabButton.removeClass('tabs__nav_active');
 		$(this).addClass('tabs__nav_active');
 		tabs.removeClass('tabs__item_active');
 		$("#"+$(this).data('tab')).addClass('tabs__item_active');
+		$('.js-functions').removeClass('functions_green');
+		$('.js-functions').removeClass('functions_red');
+		$('.js-functions').removeClass('functions_orange');
+		$('.js-functions').removeClass('functions_violet');
+		$('.js-functions').addClass('functions_'+$(this).data('color'))
+		location.hash = $(this).data('hash');
 	});
+	switch(location.hash){
+		case '#events':
+			tabButton.eq(1).trigger('click');
+			break;
+		case '#parents':
+			tabButton.eq(2).trigger('click');
+			break;
+		case '#corporate':
+			tabButton.eq(3).trigger('click');
+			break;
+	}
 });
