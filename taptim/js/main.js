1240e34d811e029b41221096f6dd9ca5d1a3f616
Manar Kurmanov <kurmanov.manar@gmail.com>
2015-12-06 23:30:00 +0600

    taptim mobile

--- taptim/js/main.js
+++ taptim/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var navActive = false;
     $('.js-scroll').click(function(event) {
         event.preventDefault();
         scrollPage($(this).attr('href'));
@@ -11,4 +12,28 @@ $(document).ready(function() {
             scrollTop: $(section).offset().top - 130
         });
     }
+    $(document).mouseup(function (e)
+    {
+        var container = $('.js-nav-trigger');
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            $('.js-nav').removeClass('nav_show');
+            $('.js-nav-trigger').removeClass('nav__trigger_active');
+           navActive=false;
+        }
+    });
+    $('.js-nav-trigger').click(function(event) {
+        event.stopPropagation();
+        if(!navActive){
+            $('.js-nav').addClass('nav_show');
+            $(this).addClass('nav__trigger_active');
+            navActive = true;
+        }else{
+            $('.js-nav').removeClass('nav_show');
+            $(this).removeClass('nav__trigger_active');
+            navActive = false;
+        }
+    });
 });
