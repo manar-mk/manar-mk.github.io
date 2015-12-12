40ace169b4f7af1ba305059460bfcf39252094bc
Manar Kurmanov <kurmanov.manar@gmail.com>
2015-12-13 02:34:00 +0600

    taptim mobile fixes

--- taptim/js/main.js
+++ taptim/js/main.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
         event.preventDefault();
         scrollPage($(this).attr('href'));
     });
-     $(".js-fancybox").fancybox({
+    $(".js-fancybox").fancybox({
         padding: 0
     });
+
     function scrollPage(section) {
         $("html, body").animate({
             scrollTop: $(section).offset().top - 130
         });
     }
-    $(document).mouseup(function (e)
-    {
+    $(document).mouseup(function(e) {
         var container = $('.js-nav-trigger');
 
         if (!container.is(e.target) // if the target of the click isn't the container...
@@ -21,19 +21,30 @@ $(document).ready(function() {
         {
             $('.js-nav').removeClass('nav_show');
             $('.js-nav-trigger').removeClass('nav__trigger_active');
-           navActive=false;
+            navActive = false;
         }
     });
     $('.js-nav-trigger').click(function(event) {
         event.stopPropagation();
-        if(!navActive){
+        if (!navActive) {
             $('.js-nav').addClass('nav_show');
             $(this).addClass('nav__trigger_active');
             navActive = true;
-        }else{
+        } else {
             $('.js-nav').removeClass('nav_show');
             $(this).removeClass('nav__trigger_active');
             navActive = false;
         }
+    });
+    $('.js-poll').click(function(event) {
+        event.preventDefault();
+        swal({
+            title: "Спасибо!",
+            text: "Ваше мнение очень важно для нас",
+            type: "success"
+        });
+    });
+    $('.js-description-trigger').click(function(event) {
+        $(this).parent().toggleClass('instruction__description_hide');
     });
 });
--- taptim/js/slider.js
+++ taptim/js/slider.js
@@ -1,38 +1,46 @@
 $(document).ready(function() {
-	var slider = $('.js-slider');
-	var sliderItems = $('.js-slider-item');
-	var sliderButton = $('.js-slider-button');
-	var sliderCurrent = 0;
+    var slider = $('.js-slider');
+    var sliderItems = $('.js-slider-item');
+    var sliderButton = $('.js-slider-button');
+    var sliderCurrent = [];
 
-	sliderItems.each(function(index, slide) {
-		$(slide).css('width', 100/sliderItems.length+'%');
-	});
 
-	slider.css('width', sliderItems.length*100+'%');
-
-	sliderButton.click(function(event) {
-		event.preventDefault();
-		sliderCurrent = $(this).data('slide');
-		sliderUpdate();
-	});
-	function sliderUpdate(){
-		sliderButton.removeClass('active');
-		sliderButton.eq(sliderCurrent).addClass('active');
-		slider.css('left', -100*sliderCurrent+'%');
-	}
-	slider.swipe({
-        swipeLeft:function(event, direction, distance, duration, fingerCount) {
-          	if(sliderCurrent<sliderItems.length-1){
-				sliderCurrent++;
-				sliderUpdate()
-          	}
-			
-        },
-        swipeRight:function(event, direction, distance, duration, fingerCount) {
-          	if(sliderCurrent>0){
-				sliderCurrent--;
-				sliderUpdate();
-          	}
-        },
+    slider.each(function(index, el) {
+        $(el).css('width', $(el).children().length * 100 + '%');
+        $(el).children().each(function(slideIndex, slide) {
+            $(slide).css('width', 100 / $(el).children().length + '%');
+        });
+        sliderCurrent[index] = 0;
+        $(el).swipe({
+            swipeLeft: function(event, direction, distance, duration, fingerCount) {
+                if (sliderCurrent[index] < $(el).children().length - 1) {
+                    sliderCurrent[index]++;
+                    sliderUpdate(index);
+                }
+            },
+            swipeRight: function(event, direction, distance, duration, fingerCount) {
+                if (sliderCurrent[index] > 0) {
+                    sliderCurrent[index]--;
+                    sliderUpdate(index);
+                }
+            },
+        });
     });
+
+    sliderButton.click(function(event) {
+        event.preventDefault();
+        sliderCurrent[$(this).data('slider')] = $(this).data('slide');
+        sliderUpdate($(this).data('slider'), $(this));
+    });
+
+    function sliderUpdate(sliderID) {
+        buttons = sliderButton.map(function(index, elem) {
+            if ($(elem).data('slider') == sliderID)
+                return this;
+        })
+        buttons.removeClass('active');
+        buttons.eq(sliderCurrent[sliderID]).addClass('active');
+        slider.eq(sliderID).css('left', -100 * sliderCurrent[sliderID] + '%');
+    }
+
 });
