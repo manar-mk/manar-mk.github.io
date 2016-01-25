5256917cf600314ff3eccf8ea39094648d20dda8
Manar Kurmanov <kurmanov.manar@gmail.com>
2016-01-25 22:23:22 +0600

    maps added

--- taptim-web/js/scripts.js
+++ taptim-web/js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-	$('#login-form').submit(function(event) {
-		event.preventDefault();
-		location.href="index.html"
-	});
+    $('#login-form').submit(function(event) {
+        event.preventDefault();
+        location.href = "index.html"
+    });
+    L.mapbox.accessToken = 'pk.eyJ1IjoibWFuYXIiLCJhIjoiY2lqbGV1cmJkMDAyZ3ZhbHhiOHE1Y3hpNCJ9.NUDL-B5daGlxnZ1WC5bTxQ';
+	if($('#map').length > 0){
+		var map = L.mapbox.map('map', 'mapbox.streets')
+    			.setView([43.2120559, -76.899596], 20);
+	}
+
+	if($('#map1').length > 0){
+    	var map = L.mapbox.map('map1', 'mapbox.streets')
+    		.setView([43.2120559, -76.899596], 20);
+	}
+	if($('#map2').length > 0){
+    	var map = L.mapbox.map('map2', 'mapbox.streets')
+    		.setView([43.2120559, -76.899596], 20);    
+	}
+	if($('#map3').length > 0){
+	    var map = L.mapbox.map('map3', 'mapbox.streets')
+    		.setView([43.2120559, -76.899596], 20);
+    }
 });
